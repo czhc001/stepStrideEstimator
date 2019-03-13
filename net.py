142eb5538bc2bc0998e55429300f529e60966de5
czhc001 <1004385883@qq.com>
2019-03-13 10:36:08 +0800

    1

--- net.py
+++ net.py
@@ -6,13 +6,14 @@ CHANNEL0 = 64
 WIDTH0 = 6
 HEIGHT0 = 256
 
-FULLY_WIDTH0 = 128
+FULLY_WIDTH0 = 32
 
-FULLY_WIDTH1 = 1
+FULLY_WIDTH1 = 2
 
 
 def inference(inputs):
-    net = slim.conv2d(inputs, CHANNEL0, [WIDTH0, HEIGHT0])
-    net = slim.fully_connected(net, FULLY_WIDTH0, scope="fully_connected0", activation_fn=tf.nn.relu)
-    net = slim.fully_connected(net, FULLY_WIDTH1, scope="fully_connected0", activation_fn=tf.nn.sigmoid)
+    # net = tf.reshape(net, [-1, ])
+    # net = slim.conv2d(inputs, CHANNEL0, [WIDTH0, HEIGHT0])
+    net = slim.fully_connected(inputs, FULLY_WIDTH0, scope="fully_connected0", activation_fn=tf.nn.relu)
+    net = slim.fully_connected(net, FULLY_WIDTH1, scope="fully_connected1", activation_fn=None)
     return net
