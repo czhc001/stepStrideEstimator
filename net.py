bc460f6e7a204466ad4543e3989c88643445518f
czhc001 <1004385883@qq.com>
2019-03-14 12:29:44 +0800

    1

--- net.py
+++ net.py
@@ -6,7 +6,7 @@ CHANNEL0 = 64
 WIDTH0 = 6
 HEIGHT0 = 256
 
-FULLY_WIDTH0 = 16
+FULLY_WIDTH0 = 2
 
 FULLY_WIDTH1 = 2
 
@@ -18,6 +18,21 @@ def inference(inputs):
                         weights_regularizer=slim.l2_regularizer(0.0005)):
         # net = tf.reshape(net, [-1, ])
         # net = slim.conv2d(inputs, CHANNEL0, [WIDTH0, HEIGHT0])
-        net = slim.fully_connected(inputs, FULLY_WIDTH0, scope="fully_connected0", activation_fn=tf.nn.relu)
-        net = slim.fully_connected(net, FULLY_WIDTH1, scope="fully_connected1", activation_fn=None)
+
+        inputs_slice = []
+        net_slice = []
+        weights_rnn = tf.Variable(tf.random_normal([8, 1], stddev=1, dtype=tf.float64), name='w')
+        biases_rnn = tf.Variable(tf.random_normal([1, 1], stddev=0, dtype=tf.float64, mean=0.1), name='b')
+
+        for i in range(5):
+            # ns = tf.strided_slice(inputs, [0, i * 8], [0, i * 8 + 8])
+            ns = inputs[:, i * 8:i * 8 + 8]
+            inputs_slice.append(ns)
+            # print(ns.get_shape())
+            # print(weights_rnn.get_shape())
+            # print(biases_rnn.get_shape())
+            net_slice.append(tf.matmul(ns, weights_rnn) + biases_rnn)
+
+        net = tf.concat([net_slice[i] for i in range(5)], 1)
+        net = slim.fully_connected(net, FULLY_WIDTH1, scope="fully_connected2", activation_fn=None)
         return net
--- train.py
+++ train.py
@@ -7,9 +7,14 @@ data = []
 label = []
 
 for i in range(1000):
-    d = np.random.normal(0, 1, size=4)
+    d = np.random.normal(0, 1, size=40)
     data.append(d)
-    if d[0] + d[2] >= d[1] + d[3]:
+    sum0 = 0
+    sum1 = 0
+    for j in range(20):
+        sum0 += d[j * 2]
+        sum1 += d[j * 2 + 1]
+    if sum0 >= sum1:
         label.append([1, 0])
     else:
         label.append([0, 1])
@@ -20,14 +25,14 @@ label = np.array(label)
 print(data.shape)
 print(label.shape)
 
-X = tf.placeholder(dtype=tf.float64, shape=[None, 4], name='X')
+X = tf.placeholder(dtype=tf.float64, shape=[None, 40], name='X')
 Y = tf.placeholder(dtype=tf.float64, shape=[None, 2], name='Y')
 
 global_step = tf.Variable(0, trainable=False)
 
 STARTER_LEARNING_RATE = 0.1
-DECAY_STEPS = 10
-DECAY_RATE = 0.96
+DECAY_STEPS = 50
+DECAY_RATE = 0.98
 MOVING_AVERAGE_DECAY = 0.99
 
 
@@ -47,26 +52,26 @@ variables_averages_op = variable_averages.apply(tf.trainable_variables())
 
 print(tf.trainable_variables())
 graph = tf.get_default_graph()
-weights0 = graph.get_tensor_by_name('fully_connected0/weights:0')
-biases0 = graph.get_tensor_by_name('fully_connected0/biases:0')
-weights0_avg = variable_averages.average(weights0)
-biases0_avg = variable_averages.average(biases0)
+for var in tf.global_variables():
+    print(var.name)
+graph = tf.get_default_graph()
+weights = graph.get_tensor_by_name('w:0')
+# weights0_avg = graph.get_tensor_by_name('fully_connected0/weights/ExponentialMovingAverage:0')
+# biases0_avg = variable_averages.average(biases0)
 
 acc = []
 
 with tf.control_dependencies([opt_op]):
     training_op = tf.group(variables_averages_op)
 with tf.Session() as sess:
+    writer = tf.summary.FileWriter("logs/", sess.graph)
     sess.run(tf.global_variables_initializer())
-    for epoch in range(300):
+    for epoch in range(3000):
         a = sess.run(accuracy, feed_dict={X: data[700:-1, :], Y: label[700:-1, :]})
         acc.append(a)
         print(str(epoch) + '  ' + str(a))
         sess.run(training_op, feed_dict={X: data[0:700, :], Y: label[0:700, :]})
-    w0 = sess.run(weights0)
-    w0_ = sess.run(weights0_avg)
-    print(w0)
-    print(w0_)
+    print(sess.run(weights))
 
 acc = np.array(acc)
 plt.plot([i for i in range(acc.shape[0])], acc)
