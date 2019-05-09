4814427ad3ed4b720947e4280beebff2897ff977
czhc001 <1004385883@qq.com>
2019-05-09 23:55:15 +0800

    1

--- tt.py
+++ tt.py
@@ -19,6 +19,7 @@ sess.run(tf.global_variables_initializer())
 X = sess.graph.get_tensor_by_name('X:0')
 P = sess.graph.get_tensor_by_name('P:0')
 op = sess.graph.get_tensor_by_name('output/BiasAdd:0')
+last_t = 0
 
 
 class MyThreadingHTTPServer(socketserver.ThreadingMixIn, HTTPServer):
@@ -27,36 +28,49 @@ class MyThreadingHTTPServer(socketserver.ThreadingMixIn, HTTPServer):
 
 class MyHttpHandler(BaseHTTPRequestHandler):
     def do_POST(self):
+        global last_t
         length = int(self.headers['Content-Length'])
         readdata = self.rfile.read(length).decode('utf-8')
-        post_data = json.loads(readdata)
-        cur_thread = threading.current_thread()
-        time.sleep(5)
-        print(1)
-        print(cur_thread.name)
-        print(post_data['Content'])
-        content = post_data['Content'].split(',')
-        feature = np.array(content, dtype=np.float)
-        print(feature.shape)
+        if readdata == 'BOARD':
+            print(readdata)
+            current_t = int(round(time.time() * 1000))
+            dt = current_t - last_t
+            print('DT ' + str(dt))
+            if last_t == 0 or current_t - last_t > 2000:
+                self.send_response(0)
+            else:
+                self.send_response(200)
+            self.end_headers()
+        else:
+            last_t = int(round(time.time() * 1000))
+            post_data = json.loads(readdata)
+            cur_thread = threading.current_thread()
+            time.sleep(5)
+            print(1)
+            print(cur_thread.name)
+            print(post_data['Content'])
+            content = post_data['Content'].split(',')
+            feature = np.array(content, dtype=np.float)
+            print(feature.shape)
 
-        r = sess.run(op, feed_dict={
-            X: [feature],
-            P: 1.0})
-        print(r)
-        __name = 'zc'
-        if r[0][0] < r[0][1]:
-            __name = 'wjw'
-        data = json.dumps({'name': __name})
-        enc = "UTF-8"
-        encoded = ''.join(data).encode(enc)
-        f = io.BytesIO()
-        f.write(encoded)
-        f.seek(0)
-        self.send_response(200)
-        self.send_header("Content-type", "text/html; charset=%s" % enc)
-        self.send_header("Content-Length", str(len(encoded)))
-        self.end_headers()
-        shutil.copyfileobj(f, self.wfile)
+            r = sess.run(op, feed_dict={
+                X: [feature],
+                P: 1.0})
+            print(r)
+            __name = 'zc'
+            if r[0][0] < r[0][1]:
+                __name = 'wjw'
+            data = json.dumps({'name': __name})
+            enc = "UTF-8"
+            encoded = ''.join(data).encode(enc)
+            f = io.BytesIO()
+            f.write(encoded)
+            f.seek(0)
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=%s" % enc)
+            self.send_header("Content-Length", str(len(encoded)))
+            self.end_headers()
+            shutil.copyfileobj(f, self.wfile)
 
 
 httpd = MyThreadingHTTPServer(('', 9601), MyHttpHandler)
